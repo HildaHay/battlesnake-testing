c1b6b9df10deb6a045a20b388b2222379ca719c4
Hilda <eskarina@eskarina.ca>
2020-08-01 23:55:07 -0700

    e

--- server.py
+++ server.py
@@ -55,13 +55,13 @@ class Battlesnake(object):
         
         move = possible_moves[0]
         
-        if x == 0 && y != height - 1:
+        if x == 0 and y != height - 1:
             move = possible_moves[1]
-        if y == 0 && x != 0:
+        if y == 0 and x != 0:
             move = possible_moves[2]
-        if x == width - 1 && y != 0:
+        if x == width - 1 and y != 0:
             move = possible_moves[0]
-        if y == height - 1 && x != width - 1:
+        if y == height - 1 and x != width - 1:
             move = possible_moves[3]
 
 
